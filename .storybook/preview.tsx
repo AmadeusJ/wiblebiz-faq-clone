9ec624547aa0b8d4ceb2f05a4fef421d2b82909d
Amadeus J <dawoonjung1026@gmail.com>
2025-02-21 09:58:06 +0900

    style: WibleBiz 전체 Style 적용 - Style은 기존의 것을 전체 가져다 쓰고, 후순위로 리팩토링

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,13 +1,14 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import type { Preview } from '@storybook/react';
-import GlobalStyles from '../src/styles/GlobalStyles';
+// import GlobalStyles from '../src/styles/GlobalStyles';
+import WibleBizStyles from '../src/styles/WibleBizStyles';
 import theme from '../src/styles/theme';
 
 const preview: Preview = {
   decorators: [
     (Story) => (
       <ThemeProvider theme={theme}>
-        <Global styles={GlobalStyles} />
+        <Global styles={WibleBizStyles} />
         <Story />
       </ThemeProvider>
     ),
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import router from '@/router';
-import GlobalStyles from '@/styles/GlobalStyles';
+// import GlobalStyles from '@/styles/GlobalStyles';
+import WibleBizStyles from '@/styles/WibleBizStyles';
 import theme from '@/styles/theme';
 import { Global, ThemeProvider } from '@emotion/react';
 import React from 'react';
@@ -10,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <ThemeProvider theme={theme}>
-        <Global styles={GlobalStyles} />
+        <Global styles={WibleBizStyles} />
         <RouterProvider router={router} />
       </ThemeProvider>
     </HelmetProvider>
